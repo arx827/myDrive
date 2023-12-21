543a8c260913b27d35d4cf10e6f1a336dc7990bd
Eason <eason.yc.chen@fubon.com>
2023-12-21 13:15:29 +0800

    [COPY] 『SDK_ok』F2B

---
+++ COPY/專案/SDK_ok/F2B/F2bre-web/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all', // 在物件或陣列最後一個元素後面是否加逗號
+  bracketSpacing: true, // 物件中列印空格
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false, // 在jsx中把'>' 放同一行
+  insertPragma: false, // 插入@format標識
+  ignorePath: '.prettierignore',
+};
